182313918c07a989f13e4a68931cb478782b1ccf
Gabrielle <ms.grodarte@gmail.com>
2024-01-11 16:34:21 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const cardMessages = []
+    for (let i = 0; i < names.length; i++) {
+        cardMessages[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    }
+    return cardMessages
+}
+
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n)
+        n--
+    }
+}
